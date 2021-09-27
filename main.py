4590e96f8e18aa3d3e8031015ef700a3b79318e3
Yum-git <yzk.yuma@gmail.com>
2021-09-28 04:44:32 +0900

    Add countsを叩くと仮データを返すようにしました

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+class Count(BaseModel):
+    argument: str
+    value: int
+
+
+class Counts(BaseModel):
+    results: List[Count]
 
 
 @app.get("/")
@@ -9,8 +30,17 @@ async def root():
 
 
 @app.get("/counts")
-async def read_count(type_key: str):
-    if type_key == 'squat':
+async def read_count(PoseName: str, response: Response):
+    """
+    過去のカウント数を取得する関数
+    Query
+    type_key: 運動タイプ
+    month_key: 月
+
+    Header
+    Bearer Token: GoogleJWTToken
+    """
+    if PoseName == 'Squat':
         response_data = {
             "results": [
                 {"argument": "01-03", "value": 20},
@@ -24,7 +54,7 @@ async def read_count(type_key: str):
             ],
             "status": 200
         }
-    elif type_key == 'pushup':
+    elif PoseName == 'PushUp':
         response_data = {
             "results": [
                 {"argument": "01-03", "value": 30},
@@ -42,5 +72,6 @@ async def read_count(type_key: str):
         response_data = {
             "status": 404
         }
+        response.status_code = 404
 
     return response_data
