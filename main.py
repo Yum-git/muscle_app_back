2befaee6071c8a192ed998eb03f79ffe80c260aa
Yum-git <yzk.yuma@gmail.com>
2021-09-28 01:42:29 +0900

    Add 仮データ追加

--- main.py
+++ main.py
@@ -4,5 +4,43 @@ app = FastAPI()
 
 
 @app.get("/")
-def root_path():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/counts")
+async def read_count(type_key: str):
+    if type_key == 'squat':
+        response_data = {
+            "results": [
+                {"argument": "01-03", "value": 20},
+                {"argument": "01-04", "value": 10},
+                {"argument": "01-05", "value": 0},
+                {"argument": "01-06", "value": 10},
+                {"argument": "01-07", "value": 30},
+                {"argument": "01-08", "value": 0},
+                {"argument": "01-09", "value": 10},
+                {"argument": "01-10", "value": 30}
+            ],
+            "status": 200
+        }
+    elif type_key == 'pushup':
+        response_data = {
+            "results": [
+                {"argument": "01-03", "value": 30},
+                {"argument": "01-04", "value": 40},
+                {"argument": "01-05", "value": 10},
+                {"argument": "01-06", "value": 0},
+                {"argument": "01-07", "value": 0},
+                {"argument": "01-08", "value": 20},
+                {"argument": "01-09", "value": 30},
+                {"argument": "01-10", "value": 0}
+            ],
+            "status": 200
+        }
+    else:
+        response_data = {
+            "status": 404
+        }
+
+    return response_data
